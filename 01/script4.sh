d0e6ef68a4b8ef2314263f1c21cd9620f1f4f52e
virginiayjd7 <a.janethrocketmail@gmail.com>
2022-08-30 23:49:16 -0500

    ➕ADD : Se resuelve el RETO1 del script4

--- 01/script4.sh
+++ 01/script4.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-mkdir foo
-mkdir foo/dummy
-mkdir foo/empty
+echo "descarga"
+wget https://es.wikipedia.org/wiki/DevOps
 
-touch foo/dummy/file2.txt 
-
-echo "Me encanta bash!!" > foo/dummy/file1.txt 
-
-mv foo/dummy/file2.txt  foo/empty
-
-echo "Me encanta bash!!" > foo/empty/file2.txt 
-
-cat foo/dummy/file1.txt foo/empty/file2.txt
+grep -w  --color -n meta DevOps 
